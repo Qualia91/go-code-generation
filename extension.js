a29fc2cadb0c76c0d83054784a6d98c075bf2d11
Nickolas.Wood <{ID}+{username}@users.noreply.github.com>
2021-03-26 12:11:53 +0000

    regex to finid structs and info in struct in go file working

--- extension.js
+++ extension.js
@@ -1,3 +1,5 @@
+'use strict'
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
@@ -18,13 +20,49 @@ function activate(context) {
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('go-code-generation.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Go Code Generation!');
+		// check file is open
+		var editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showErrorMessage("No file open")
+			return;
+		}
+
+		// check we have a go file open
+		if (!editor.document.fileName.endsWith(".go")) {
+			vscode.window.showErrorMessage("File is not a Go file")
+			return;
+		}
+
+		// get all text in file
+		var file_text = editor.document.getText()
+
+		const regex = /type +([^ ]+) +struct {(.+?(?=}))}/gs;
+
+		// look for structs
+		var struct_names = funcStructs(file_text, regex)
+
+		struct_names.forEach(value => vscode.window.showWarningMessage(value))
+
+		
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function funcStructs(file_text, regex) {
+	var all_matched = file_text.matchAll(regex)
+
+	var struct_names = []
+	var struct_contents = []
+
+	for (const iterator of all_matched) {
+		struct_names.push(iterator[1])
+		struct_contents.push(iterator[2])
+	}
+
+	
+	return struct_names
 }
 
 // this method is called when your extension is deactivated
